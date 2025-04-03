37fb65ae41c1379a1d5c86b4ac3a96761dc8adb2
Cody Owens <cowens1435@gmail.com>
2025-04-03 14:24:08 -0600

    In class work

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,13 +1,49 @@
 #include "Student.h"
-
+#include <string>
 #include <iostream>
 
 using namespace std;
+
+struct Person { /* Data Structure(typically holds only data) */
+
+    /* Default access level (PUBLIC) */
+
+    float weight;
+
+    string name;
+    int age;
+};
 
 int main() {
     f();
     Student student("That guy");
     student.Write();
+
+    Person person;
+    person.age = 19;
+
+    unsigned int i = -23;
+    printf("%d\n", i); // curious how it prints it as negative here
+    cout << i << endl;
+    cout << sizeof(int) << endl;
+
+    char c = 'A';
+    cout << (int) c << endl;
+
+    cout << sizeof(string) << endl;
+
+    cout << sizeof(Person) << endl;
+
+    cout << (char) 67 << endl; // Letter 'C'
+    cout << &i << endl; // Address
+
+    i++; // All arithmetic operators exist
+    i += 5;
+
+    cout << i << endl;
+    i++; // Post-fix: Do the operation after
+    ++i; // Pre-fix: Do the operation prior
+
     cout << "Hello, World!" << endl;
     cout << "Git is now tracking this project!" << endl;
     return 0;
